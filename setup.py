4ab2c7b6a3c7adc920a69571efc78fcb157c3c1e
cswu <xray0h@gmail.com>
2017-12-12 15:58:09 +0800

    fix module issue

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='mimi',
-    version='1.0.0b',
-    py_modules=['mimi'],  # 將模組的metadata關連到setup函式
+    version='1.0.0b4',
+    py_modules=['mimi','mode','output','preprocess','generator'],  # 將模組的metadata關連到setup函式
     author='exeex',
     author_email='xray0h@gmail.com',
     url='....',
