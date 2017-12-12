6a636ebf47536e331af4e9950147feb3decf3fb2
cswu <xray0h@gmail.com>
2017-12-12 17:41:26 +0800

    change file structure 2

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(
     name='mimi',
-    version='1.0.0b4',
-    py_modules=['mimi','mode','output','preprocess','generator'],  # 將模組的metadata關連到setup函式
+    version='1.0.0b11',
+    packages=['mimi'],
+    # py_modules=['mimi','mode','output','preprocess','generator'],  # 將模組的metadata關連到setup函式
     author='exeex',
     author_email='xray0h@gmail.com',
     url='....',
