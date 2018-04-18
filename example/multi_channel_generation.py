ebe814ac2a2b3f0190b9bc600ccd6ea09bd53686
cswu <xray0h@gmail.com>
2018-04-18 17:00:40 +0800

    change example

--- example/multi_channel_generation.py
+++ example/multi_channel_generation.py
@@ -1,9 +1,13 @@
 from mimi import MidiFile, MidiTrack, generator
+from mimi.instrument import Piano, Strings
 
-tracks = [MidiTrack(channel=x,instrument=(x+1)*8) for x in range(3)]
+
+tracks = [MidiTrack(channel=0, instrument=Piano.AcousticGrandPiano),
+          MidiTrack(channel=1, instrument=Strings.Cello)]
 
 for track in tracks:
-    track.append_bar(generator.get_random_tab(tempo=70))
+    track.append_bar(generator.get_random_tab(tempo=70, key="C"))
+
 
 mid = MidiFile()
 mid.tracks.extend(tracks)
