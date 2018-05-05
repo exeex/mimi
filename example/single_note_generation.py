35adf5845a1a4791182bae7c2afcf2015c5769b0
cswu <xray0h@gmail.com>
2018-05-05 13:19:44 +0800

    add example

---
+++ example/single_note_generation.py
@@ -0,0 +1,25 @@
+from mimi import MidiFile, MidiTrack, generator
+from mimi.Mimi import AbsNote
+from mimi.instrument import Piano, Strings
+
+
+
+
+track = MidiTrack(channel=0, instrument=Piano.AcousticGrandPiano)
+
+
+for x in range(40, 76):
+    track.append(AbsNote(pitch=x,time=256))
+
+# 把track放到MidiFile物件中
+mid = MidiFile()
+mid.tracks.append(track)
+
+# 對mid物件進行存檔/播放等操作
+mid.draw_roll()
+mid.play()
+
+# roll = mid.get_roll()
+# mid.save_npz("test.npz")
+# mid.save_png("test.png")
+# mid.save_mp3("test.mp3")
