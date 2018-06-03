c51349c2d267b3dce664d57595b4c893f6d1f4e7
cswu <xray0h@gmail.com>
2018-06-03 10:37:37 +0800

    fix bug, add drum

--- example/mimi_c8_3.py
+++ example/mimi_c8_3.py
@@ -13,9 +13,9 @@ if not os.path.exists(mp3_folder):
 if not os.path.exists(npz_folder):
     os.mkdir(npz_folder)
 
+
 for x in range(1):
 
-    choice = np.random.choice(8, 3, replace=False)
     tracks = [MidiTrack(channel=0, instrument=Piano.AcousticGrandPiano),
               MidiTrack(channel=1, instrument=Guitar.ElectricGuitar_jazz),
               MidiTrack(channel=2, instrument=Strings.Cello),
@@ -25,18 +25,21 @@ for x in range(1):
               MidiTrack(channel=6, instrument=SynthLead.Lead2_sawtooth),
               MidiTrack(channel=7, instrument=Guitar.OverdrivenGuitar)]
 
-    # drum = MidiTrack(channel=9)
+    drum = MidiTrack(channel=9)
 
-    track = [tracks[i] for i in choice]
+    choice = np.random.choice(8, 3, replace=False)
+    tracks_ = [tracks[i] for i in choice]
+    # tracks_.append(drum)
 
-    for track in tracks:
+    for track in tracks_:
         track.append(generator.get_random_tab(tempo=70))
 
     mid = MidiFile()
-    mid.tracks.extend(tracks)
+    mid.tracks.extend(tracks_)
     # mid.draw_roll()
 
     # mid.save_mp3("data/mp3/%03d.mp3" % x)
     # mid.save_npz("data/npz/%03d.npz" % x)
 
     mid.play()
+    print(tracks_)
